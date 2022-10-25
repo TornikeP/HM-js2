e3b27af99a69b8ba8d11c576b716c947b05e7994
TornikeP <112972102+TornikeP@users.noreply.github.com>
2022-10-25 19:14:02 +0400

    eh....

---
+++ script.js
@@ -0,0 +1,65 @@
+alert("დევიწყეთ სმნ")
+
+var numbers = [6,12,18,24,30,999];
+for(var i=0; i <numbers.length; i++) {
+  document.write(numbers[i]);
+}
+
+
+
+
+
+
+
+// function price(p){
+//     if(p<=20) { 
+//         return("იაფი");
+//     }
+//     else if(p>20 && p<=50){
+//         return("საშუალო");
+//     }
+//     else{
+//         return("ძვირია");
+//     }
+// }
+// var result = price(500);
+// document.write(result)
+
+
+
+
+// var strSum=prompt("თანხა")
+// function money() {
+//     strSum;
+// }
+
+// function price() {
+//     Money();
+//     if(strSum<20) {
+//         document.write("იაფი")
+//     }
+//     else if(strSum>20 && strSum<50) {
+//         document.write("საშუალო")
+//     }
+//     else{
+//         document.write("ძვირი")
+//     }
+// }
+//  price();
+ 
+
+
+
+
+
+
+
+var numbers =[15,53,22,198,10,28,16,70,33,951];
+for(var i=0; i < numbers.length; i++) {
+    if (numbers[i]%2==1){
+        document.write(numbers[i]+" ");
+    }
+}
+
+
+alert("ეჰ არადა რა შემართებით დევიწყეთ სმნ")
